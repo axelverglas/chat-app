7aadf3e707f31d6c3ba68a9afbb2806c98bec60e
Axel Verglas <axelverglas@outlook.com>
2022-04-22 17:54:22 +0200

    code source

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import axios from 'axios';
+
+const LoginForm= () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const authObject = { 'Project-ID': process.env.REACT_APP_PROJECT_ID, 'User-Name': username, 'User-Secret': password };
+
+    try {
+      await axios.get('https://api.chatengine.io/chats', { headers: authObject });
+
+      localStorage.setItem('username', username);
+      localStorage.setItem('password', password);
+
+      window.location.reload();
+      setError('');
+    } catch (err) {
+      setError('Oops, informations incorrect !');
+    }
+  };
+
+  return (
+    <div className="wrapper">
+      <div className="form">
+        <h1 className="title">Chat Application</h1>
+        <form onSubmit={handleSubmit}>
+          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className="input" placeholder="Nom d'utilisateur" required />
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="input" placeholder="Mot de passe" required />
+          <div align="center">
+            <button type="submit" className="button">
+              <span>Connexion</span>
+            </button>
+          </div>
+        </form>
+        <h1>{error}</h1>
+      </div>
+    </div>
+
+  );
+};
+
+export default LoginForm;
